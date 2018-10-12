3e8b6314944825a138af20329295eed4871799ef
Nap14hockey <Nap14hockey@users.noreply.github.com>
2018-10-11 20:52:57 -0400

    Update HelloWorld_Plugin.java
    Created Helloworld Command

--- src/HelloWorld_Plugin.java
+++ src/HelloWorld_Plugin.java
@@ -3,7 +3,8 @@
  */
 
 import java.util.logging.Logger;
-
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HelloWorld_Plugin extends JavaPlugin {
@@ -20,7 +21,13 @@ public class HelloWorld_Plugin extends JavaPlugin {
     public void onDisable() {
         log.info("[HelloWorld_Plugin] Stopping...");
     }
-
+public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("helloworld")) {
+            if (sender instanceof Player){
+                Player player = (Player) sender;
+                player.sendMessage(ChatColor.GREEN + "Hello World!");
+            }
+         
 
 
 
